c151b22bfd877ff520ba0790688b2462b1fe51f9
Andrew Poydence <apoydence@gmail.com>
2015-06-09 07:01:39 -0600

    Sets up main

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"github.com/apoydence/hermes/handlers"
+	"net/http"
+)
+
 func main() {
+	keyStorer := handlers.NewKeyStorer()
+	uploader := handlers.NewUploader(keyStorer)
+	downloader := handlers.NewDownloader(keyStorer)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.RequestURI != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if req.Method == "GET" {
+			downloader.ServeHTTP(w, req)
+		} else if req.Method == "POST" {
+			uploader.ServeHTTP(w, req)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
